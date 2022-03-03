7f7801320a914f3c0b5355cb753c5c059d5f84e7
Nick McMurdie <nickmcmurdie@gmail.com>
2022-03-02 21:29:34 -0700

    Test vibration on hurt

--- logic.js
+++ logic.js
@@ -305,7 +305,7 @@ function hurtPlayer(damage) {
 
    animateDamage(computedDamage === 0);
    if (computedDamage) {
-      navigator.vibrate(100);
+      navigator.vibrate(200);
       updateHUDElem("health", Math.max(game.health - computedDamage, 0), true);
    }
 
